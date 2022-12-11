61d6362af3a847d107b7fe4ca3b8ce243a2271ae
Anna Mattingly <115038514+anna-mattingly@users.noreply.github.com>
2022-12-11 16:57:43 -0600

    Digital Clock

---
+++ Digital Clock.py
@@ -0,0 +1,27 @@
+# importing whole module
+from tkinter import *
+from tkinter.ttk import *
+
+# importing strftime funciton to retrieve system's time
+from time import strftime
+
+# creating tkinter window
+
+root = Tk()
+root.title('Clock')
+
+# Using this function to display time on the label
+def time(): 
+    string = strftime('%H:%M:%S %p') 
+    lbl.config(text = string) 
+    lbl.after(1000, time)
+
+# Styling the label widget so that clock will look more attractive! 
+lbl = Label(root, font = ('calibri', 40, 'bold'), 
+            background = 'black', 
+            foreground = 'white') 
+            
+# Placing clock at the center ofthe tkinter window            
+lbl.pack(anchor = 'center') 
+time()  
+mainloop()
